80d6ec671f394e68757922578de65aa369613263
Francisco Rodriguez <168039749+franciscolrm86@users.noreply.github.com>
2024-06-03 19:59:08 -0400

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -4,3 +4,19 @@ resource "aws_s3_bucket" "bucket1" {
         name="test-bucket-${count.index}"
         }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+}
+ 
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+ 
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
